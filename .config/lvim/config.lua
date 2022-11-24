a0013542ed2f321329b5904d1e6f96326f8f25ef
fseixas <fseixas@pm.me>
2022-11-24 00:28:21 -0300

    refactor

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -32,7 +32,6 @@ lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
-
 local _, actions = pcall(require, "telescope.actions")
 lvim.builtin.telescope.defaults.mappings = {
   -- for input mode
@@ -170,12 +169,13 @@ lvim.builtin.treesitter.highlight.enable = true
 lvim.plugins = {
 
   { "mateusbraga/vim-spell-pt-br" },
-  { "dracula/vim" },
 
+  { "dracula/vim" },
   { "Mofiqul/adwaita.nvim" },
   { "sainnhe/gruvbox-material" },
   { "sainnhe/everforest" },
   { "catppuccin/nvim" },
+
   { "tpope/vim-surround" },
   { "tpope/vim-repeat" },
 
